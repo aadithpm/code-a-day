38de053b7756432fe4ddec815df4fa1b423c0e36
aadithpm <aadithpmdesigns@gmail.com>
2017-07-08 22:29:38 +0530

    Added Simple Fun #166: Best Match

---
+++ py/Simple Fun #166: Best Match.py
@@ -0,0 +1,8 @@
+"""
+https://www.codewars.com/kata/simple-fun-number-166-best-match/python
+"""
+def best_match(goals1, goals2):
+  goals = [[i - j, j] for i,j in zip(goals1,goals2)]
+  _min = min([i[0] for i in goals])
+  _max = max([i[1] for i in goals if i[0] == _min])
+  return goals.index([_min,_max])  
