90786d7a2aa89068ca35384cd4c7c2c168d603bf
aadithpm <aadithpm@gmail.com>
2019-02-25 11:02:37 -0600

    Added solution for Consecutive Strings

---
+++ py/Consecutive Strings.py
@@ -0,0 +1,11 @@
+def longest_consec(strarr, k):
+    if k < 1 or k > len(strarr) or len(strarr) == 0:
+        return ''
+    cur_len = 0
+    cur_string = ""
+    for idx, val in enumerate(strarr[:len(strarr) - k + 1]):
+        temp_str = ''.join(strarr[idx: idx + k])
+        if len(temp_str) > cur_len:
+            cur_string = temp_str
+            cur_len = len(temp_str)
+    return cur_string
